26d536c9f41e046ef2aa6faea3f8a7a6acbd6df8
liwhy1 <levi.toth2005@gmail.com>
2025-08-18 15:41:18 +0200

    MT6893: init: Set gamma conversion on init
    * Also enable sys.brightness.disable_gamma_conversion by default

--- init/init_MT6893.cpp
+++ init/init_MT6893.cpp
@@ -106,4 +106,9 @@ void vendor_load_properties() {
 
     // Optional: global ro.device override
     property_override("ro.vendor.device", device.c_str());
+
+    // Set gamma conversion prop for denniz
+    if (device == "denniz") {
+        property_override("sys.brightness.disable_gamma_conversion", "0");
+    }
 }
