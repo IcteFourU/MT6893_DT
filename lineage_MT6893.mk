7cb3284b7a96724ecaf21749f0d732e9a2c3377c
IcteFourU <tanovskijdima@gmail.com>
2025-09-30 14:48:49 +0300

    Revert "MT6893: Bringup Evolution-X"
    This reverts commit 6dbd98b639c087b752b9e5f5528e9447ce0923b4.

--- lineage_MT6893.mk
+++ lineage_MT6893.mk
@@ -24,13 +24,6 @@ $(call inherit-product, device/oplus/MT6893/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# EvolutionX Flags
-EVO_BUILD_TYPE := Unofficial
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_BUILD_APERTURE_CAMERA := false
-TARGET_HAS_UDFPS := true
-TARGET_SUPPORTS_QUICK_TAP := true
-
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_MT6893
 PRODUCT_DEVICE := MT6893
