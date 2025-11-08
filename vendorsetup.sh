d4681d64ae3bf450afa459efe77a883550f743fe
liwhy1 <levi.toth2005@gmail.com>
2025-11-08 16:53:36 +0100

    MT6893: vendorsetup: Apply patches

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-# Patch wpa_supplicant
-cd external/wpa_supplicant_8
-git fetch https://github.com/liwhy1/android_external_wpa_supplicant_8 bab5ea1f459ad1f4068dea5702839ce02373b067
-git cherry-pick bab5ea1f459ad1f4068dea5702839ce02373b067
-cd -
+PATCH_DIR="device/oplus/MT6893/patches"
+
+# Restore patches
+restore_device_patches() {
+    if [ -d "$PATCH_DIR" ]; then
+        cd external/wpa_supplicant_8 && git restore . && cd -
+        cd frameworks/base && git restore . && cd -
+    fi
+}
+
+# Apply patches
+apply_device_patches() {
+    if [ -d "$PATCH_DIR" ]; then
+        for PATCH in "$PATCH_DIR"/*.patch; do
+            [ -e "$PATCH" ] || continue
+            echo "Applying $PATCH..."
+            git apply "$PATCH"
+        done
+    fi
+}
+
+restore_device_patches
+apply_device_patches
